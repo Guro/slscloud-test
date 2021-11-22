a8af80fe6c5bcb4d1fbd6405e236b96cd6b601e2
Guro Khundadze <gurrok@gmail.com>
2021-11-22 15:41:16 +0400

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,85 @@
+import { useState, useEffect } from 'react'
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios'
+
+axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? 'https://curly-rice-1ib2z.cloud.serverless.com/' : ''
+
+const Loading = () => (
+    <div>
+        <p>Loading Users...</p>
+    </div>
+)
+
+const Users = (props) => {
+    const { users } = props
+    return users.map((user) => {
+        return (
+            <div key={user.id}>
+                <strong>{user.key}</strong>, 
+                <span className={user.value.company}>Company: {` ${user.value.company}`}</span>
+            </div>
+        )
+    })
+}
+
+
+const App = () => {
+  const [loading, setLoading] = useState(false)
+  const [users, setUsers] = useState([])
+  const [newUserName, setNewUserName] = useState('');
+  const [newUserCompany, setNewUserCompany] = useState('');
+ 
+  const fetchAndSetUsers = async () => {
+    try {
+        setLoading(true)
+        const response = await axios.get('/api/users')
+        setUsers(response.data.users)
+        setLoading(false)
+    } catch (e) {
+        console.log(e)
+        setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchAndSetUsers()
+  }, [])
+
+  return (
+    <div className='App' style={{ display: 'flex', alignItems: 'center', textAlign: 'center', flexDirection: 'column', height: '100vh' }}>
+        <img height={150} width={150} src={logo} className="App-logo" alt="logo" style={{ marginBottom: -50, marginTop: -50 }} />
+        <h1>
+            Serverless Cloud, API, DataStore + react.js:
+        </h1>
+        <div>
+        {loading
+            ? <Loading/>
+            : <Users users={users} />
+        }
+        </div>
+        <div>
+            <hr />
+            <h2>Add User</h2>
+            name: <input type="text" onChange={(e)=>{
+                setNewUserName(e.target.value)
+            }} /> <br/>
+            Company: <input type="text" onChange={(e)=>{
+                setNewUserCompany(e.target.value)
+            }}/> <br/>
+            <button onClick={async ()=>{
+                await axios.post('/api/users',{
+                    "name":newUserName,
+                    "data":{
+                        "company":newUserCompany
+                    }
+                })
+                await fetchAndSetUsers();
+            }}>Add User</button>
+        </div>
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,33 @@
+import { api, data } from "@serverless/cloud";
+
+// Create GET route and return users
+api.get("/api/users", async (req, res) => {
+    // Get users from Serverless Data
+    let result = await data.get("user:*", true);
+    // Return the results
+    res.send({
+        users: result.items,
+    });
+});
+
+
+api.post("/api/users", async (req, res) => {
+    console.log('POST API/USERS')
+    await data.set(`user:${req.body.name}`, req.body.data);
+    res.send({
+        hello:"world",
+        body:req.body
+    })
+});
+
+// Catch all for missing API routes
+api.get("/api/*", (req, res) => {
+    console.log(`404 - api`);
+    res.status(404).send({ error: "not found" });
+});
+
+// Catch all for React SPA routes
+// api.get("/*", (req, res) => {
+    // // console.log(`404 - loading index.html`);
+    // res.sendFile(`${process.cwd()}/static/index.html`);
+// });
